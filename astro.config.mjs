0f3e500d74826047a8b9d8f4c60256756fe243df
hasanaliarikannn <hasanaliarikan07@hotmail.com>
2022-07-24 18:41:58 +0300

    add navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), ]
+  integrations: [preact(), ],
+  vite:{
+    ssr: {
+      external:["svgo"]
+    }
+  }
 });
